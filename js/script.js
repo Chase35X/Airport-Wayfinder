d58d85df25856884731c791b05796d73c3ead0cf
Chase35X <89099136+Chase35X@users.noreply.github.com>
2023-08-22 15:46:45 -0400

    testing with all 4

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var h = window.innerHeight;
 var hoverCount=0;
 
 var weatherAPI = "7d13921413274920be4210715231908";
-var flightAPI = "ef0b6a04867fb62317e7c65ae0fbed02";
+var flightAPI = "66dab4b1-0c5b-415f-ae72-66d9f8e93a59";
 
 // function hover(){
 
@@ -19,6 +19,10 @@ var flightAPI = "ef0b6a04867fb62317e7c65ae0fbed02";
 
 //     hoverCount++;
 // }
+
+function updateScreens(){
+    
+}
 
 
 function pullWeather(){
@@ -51,7 +55,7 @@ function pullWeather(){
 
 function pullFlight(){
     let request = new XMLHttpRequest();
-    request.open("GET", "http://www.api.aviationstack.com/v1/flights?access_key=" + flightAPI + "&flight_status=scheduled&flight_date=2023-08-19");
+    request.open("GET", "https://airlabs.co/api/v9/flights?api_key=" + flightAPI + "&airline_icao=JBU&_view=array");
     request.send();
     request.onload = () => {
         console.log(request);
@@ -59,6 +63,61 @@ function pullFlight(){
             console.log(JSON.parse(request.response));
             var data = JSON.parse(request.response);
             console.log();
+
+            
+            console.log(data);
+
+            for(var i = 0; i<4; i++){
+                var flightCode1 = data.response[i].flight_iata;
+                var arrivalAirline1 = data.response[i].arr_iata;
+                console.log(flightCode1);
+                pullFlightInfo(flightCode1);
+                airportLookup(arrivalAirline1);
+            }
+        }else{
+            console.log(`error ${request.status} ${request.statusText}`)
+        }
+    }
+}
+
+function pullFlightInfo(flightIataCode){
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://airlabs.co/api/v9/flight?flight_iata=" + flightIataCode + "&api_key=" + flightAPI);
+    request.send();
+    request.onload = () => {
+        console.log(request);
+        if(request.status==200){
+            console.log(JSON.parse(request.response));
+            var data = JSON.parse(request.response);
+            console.log();
+
+            var arrivalAirportIataCode = data.response.arr_iata;
+            console.log(arrivalAirportIataCode);
+
+            var gateNumber = data.response.dep_gate;
+            var departureTime = data.response.dep_time;
+
+            console.log(gateNumber);
+            console.log(departureTime);
+        }else{
+            console.log(`error ${request.status} ${request.statusText}`)
+        }
+    }
+}
+
+function airportLookup(airportIataCode){
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://airlabs.co/api/v9/airports?iata_code=" + airportIataCode + "&api_key=" + flightAPI);
+    request.send();
+    request.onload = () => {
+        console.log(request);
+        if(request.status==200){
+            console.log(JSON.parse(request.response));
+            var data = JSON.parse(request.response);
+            console.log();
+
+            var arrivalCity = data.response.city;
+            console.log(arrivalCity);
         }else{
             console.log(`error ${request.status} ${request.statusText}`)
         }
@@ -66,4 +125,4 @@ function pullFlight(){
 }
 
 pullWeather();
-// pullFlight();
+pullFlight();
